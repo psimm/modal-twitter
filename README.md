533796e2b667bf76c1b56b37d0d6e701cfc247ec
Paul Simmering <paul.simmering@gmail.com>
2023-01-27 14:18:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Automated Twitter API Data Collector using Modal
+
+Please refer to the accompanying [blog post](https://simmering.dev/blog/twitter-modal/) for a motivation and explanation of the code.
+
+## Installation
+
+1. Clone the repository
+2. Install the dependencies via [Poetry](https://python-poetry.org) or pip
+3. Create a [Twitter app](https://developer.twitter.com/en) and get the API keys
+4. Create an AWS S3 bucket and note the bucket name
+5. Create an IAM user with read and write access to the bucket and get the API keys
+6. Create a [Modal](https://modal.com) account, and create 2 secrets via the dashboard:
+
+Create a secret named "twitter-api" and add these variables:
+
+```
+TWITTER_ACCESS_TOKEN
+TWITTER_ACCESS_SECRET
+TWITTER_CONSUMER_KEY
+TWITTER_CONSUMER_SECRET
+```
+
+Create a secret named "aws-s3-access" and add these variables:
+
+```
+AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY
+S3_BUCKET
+```
+
+7. Adjust the words in `terms.json` to your liking and upload the file to your S3 bucket
+8. Run `python app.py` to do a test run
+9. Deploy to Modal with `modal deploy --name twitter-data-collector app.py`
---
+++ app.py
@@ -0,0 +1,102 @@
+import modal
+import twitter
+import os
+import boto3
+import json
+from datetime import datetime
+
+stub = modal.Stub(
+    image=modal.Image.debian_slim().pip_install(["boto3", "python-twitter"])
+)
+
+@stub.function(secret=modal.Secret.from_name("twitter-api"))
+def get_tweets(term: str, count: int, since_id: str = None) -> list:
+    # Authenticate with Twitter API
+    api = twitter.Api(
+        consumer_key=os.environ["TWITTER_CONSUMER_KEY"],
+        consumer_secret=os.environ["TWITTER_CONSUMER_SECRET"],
+        access_token_key=os.environ["TWITTER_ACCESS_TOKEN"],
+        access_token_secret=os.environ["TWITTER_ACCESS_SECRET"],
+    )
+
+    return api.GetSearch(
+        term=term,
+        count=count,
+        since_id=since_id,
+        lang="en",  # adjust to fetch tweets in other languages
+        result_type="recent",
+    )
+
+
+@stub.function(secret=modal.Secret.from_name("aws-s3-access"))
+def save_tweets(filename: str, tweets: list) -> str:
+    tweets_dict_list = [t.AsDict() for t in tweets]
+
+    # Save JSON to S3
+    s3 = boto3.client("s3")
+    s3.put_object(
+        Bucket=os.environ["S3_BUCKET"],
+        Key=filename,
+        Body=json.dumps(tweets_dict_list),
+    )
+
+    print(f"Saved {len(tweets)} tweets to {filename} on S3")
+
+    since_id = tweets_dict_list[-1]["id"]  # assumes tweets are sorted by id
+    return since_id
+
+
+@stub.function(secret=modal.Secret.from_name("aws-s3-access"))
+def get_terms() -> list[dict]:
+    s3 = boto3.client("s3")
+    terms = json.loads(
+        s3.get_object(Bucket=os.environ["S3_BUCKET"], Key="terms.json")["Body"].read()
+    )
+
+    # Prioritize terms that have not been searched for recently
+    terms = sorted(terms, key=lambda t: (t["timestamp_last_search"], t["since_id"]))
+    return terms
+
+
+@stub.function(secret=modal.Secret.from_name("aws-s3-access"))
+def save_terms(terms: list[dict]) -> None:
+    s3 = boto3.client("s3")
+    s3.put_object(
+        Bucket=os.environ["S3_BUCKET"],
+        Key="terms.json",
+        Body=json.dumps(terms),
+    )
+    print("Updated terms.json on S3")
+
+
+@stub.function(schedule=modal.Period(minutes=15))
+def main():
+    terms = get_terms.call()
+
+    print(f"{len(terms)} terms to search: {', '.join([t['term'] for t in terms])}")
+
+    for term in terms:
+        print(f"Searching for term: {term['term']}")
+
+        timestamp = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+        filename = f"{timestamp} {term['term']}.json".replace(" ", "_")
+
+        try:
+            tweets = get_tweets.call(term["term"], 100)  # maximum allowed by API
+        except Exception as e:
+            print(e)
+            print("Could not get tweets. Saving tweets collected so far.")
+            break
+
+        since_id = save_tweets.call(filename, tweets)  # returns since_id
+
+        # Update values in terms
+        term["since_id"] = since_id
+        term["timestamp_last_search"] = timestamp
+
+    save_terms.call(terms)
+
+
+if __name__ == "__main__":
+    with stub.run():
+        main.call()
